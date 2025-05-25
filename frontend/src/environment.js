a8e31ae8c45d492f6c04d302115b8128118b4501
Riya Joshi <joshiriya442@gmail.com>
2025-05-25 17:50:14 +0530

    Update environment.js with server URLs for prod and local

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 const IS_PROD = false; // local development mode
 
 const server = IS_PROD
-  ? "https://meetzybackend-ku9e.onrender.com" // production URL
+  ? "https://meetzy-backend1.onrender.com" // production URL
   : "http://localhost:8000"; // local server
 
 export default server;
