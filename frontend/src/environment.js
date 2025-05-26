0601c1140254cd2e111dec37ddbaa29e5fd4d3a5
Riya Joshi <joshiriya442@gmail.com>
2025-05-26 14:43:25 +0530

    Update backend and frontend with server URL changes

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
-const IS_PROD = false; // local development mode
+const IS_PROD = import.meta.env.MODE === "production";
 
 const server = IS_PROD
   ? "https://meetzy-backend1.onrender.com" // production URL
-  : "http://localhost:8000"; // local server
+  : "http://localhost:8000"; // local development
 
 export default server;
